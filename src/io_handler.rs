17c49ab8cdd220d60e5ffac3df6fa7b4968a40fc
kiipuri <kiipuri01@gmail.com>
2022-09-18 05:11:32 +0300

    add stop function to io

--- src/io_handler.rs
+++ src/io_handler.rs
@@ -1,4 +1,10 @@
-use std::time::Duration;
+use std::{
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+    time::Duration,
+};
 
 use crossterm::event::{self, poll, Event, KeyEvent};
 use log::error;
@@ -10,35 +16,43 @@ pub enum InputEvent {
 
 pub struct Events {
     rx: tokio::sync::mpsc::Receiver<InputEvent>,
-    // _tx: tokio::sync::mpsc::Sender<InputEvent>,
+    stop_capture: Arc<AtomicBool>,
 }
 
 impl Events {
     pub async fn new(tick_rate: Duration) -> Events {
-        let (_tx, rx) = tokio::sync::mpsc::channel(100);
-        let event_tx = _tx.clone();
+        let (tx, rx) = tokio::sync::mpsc::channel(100);
+        let stop_capture = Arc::new(AtomicBool::new(false));
 
+        let event_stop_capture = stop_capture.clone();
         tokio::spawn(async move {
             loop {
-                tokio::time::sleep(Duration::from_millis(1)).await;
                 if poll(tick_rate).unwrap() {
                     if let Event::Key(key) = event::read().unwrap() {
-                        if let Err(_) = event_tx.send(InputEvent::Input(key)).await {
+                        if let Err(_) = tx.send(InputEvent::Input(key)).await {
                             error!("event poll errored");
                         };
                     }
                 } else {
-                    if let Err(_) = event_tx.send(InputEvent::Tick).await {
+                    if let Err(_) = tx.send(InputEvent::Tick).await {
                         error!("event poll errored");
                     }
+                }
+
+                if event_stop_capture.load(Ordering::Relaxed) {
+                    break;
                 }
             }
         });
 
-        Events { rx }
+        Events { rx, stop_capture }
     }
 
     pub async fn next(&mut self) -> InputEvent {
         self.rx.recv().await.unwrap()
+    }
+
+    pub fn close(&mut self) {
+        self.stop_capture.store(true, Ordering::Relaxed)
     }
 }
