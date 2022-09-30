5ae4c800dc6309c4970dbb2f252721e4e89d9a02
kiipuri <kiipuri01@gmail.com>
2022-09-30 21:09:24 +0300

    Keybind to verify torrent and set column sorting

--- src/key_handlers.rs
+++ src/key_handlers.rs
@@ -3,13 +3,6 @@ use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
 use crate::app::{App, FloatingWidget, FocusableWidget, InputMode, Route, RouteId};
 
 pub async fn handler(key: KeyEvent, app: &mut App) {
-    // if !matches!(app.floating_widget, FloatingWidget::None) {
-    //     if key == KeyCode::Esc {
-    //         handle_esc(app);
-    //     }
-    //     return;
-    // }
-
     match app.input_mode {
         InputMode::Normal => match key.code {
             KeyCode::Char('K') | KeyCode::Char('k') | KeyCode::Up => handle_up(key, app),
@@ -24,6 +17,7 @@ pub async fn handler(key: KeyEvent, app: &mut App) {
             KeyCode::Char('d') => handle_remove(app).await,
             KeyCode::Char('t') => handle_toggle(app),
             KeyCode::Char('c') => handle_columns(app),
+            KeyCode::Char('v') => handle_verify(app).await,
             KeyCode::Enter => handle_enter(app).await,
             KeyCode::Esc => handle_esc(app),
             _ => (),
@@ -51,17 +45,16 @@ fn handle_up(key: KeyEvent, app: &mut App) {
             return;
         }
         FloatingWidget::AddTorrentConfirm | FloatingWidget::Help => return,
-        FloatingWidget::ModifyColumns => match key.modifiers {
-            KeyModifiers::SHIFT => {
+        FloatingWidget::ModifyColumns => {
+            if let KeyModifiers::SHIFT = key.modifiers {
                 app.move_column_up();
                 app.previous_column();
                 return;
-            }
-            _ => {
+            } else {
                 app.previous_column();
                 return;
             }
-        },
+        }
         _ => (),
     }
 
@@ -79,17 +72,16 @@ fn handle_down(key: KeyEvent, app: &mut App) {
             return;
         }
         FloatingWidget::AddTorrentConfirm | FloatingWidget::Help => return,
-        FloatingWidget::ModifyColumns => match key.modifiers {
-            KeyModifiers::SHIFT => {
+        FloatingWidget::ModifyColumns => {
+            if let KeyModifiers::SHIFT = key.modifiers {
                 app.move_column_down();
                 app.next_column();
                 return;
-            }
-            _ => {
+            } else {
                 app.next_column();
                 return;
             }
-        },
+        }
         _ => (),
     }
 
@@ -114,6 +106,14 @@ fn handle_right(app: &mut App) {
             app.floating_widget = FloatingWidget::AddTorrentConfirm;
             return;
         }
+        FloatingWidget::ModifyColumns => {
+            let column = app.all_info_columns[app.selected_column.unwrap()].column;
+            app.sort_column = column;
+            app.sort_descending = true;
+            log::error!("{:?}", app.sort_column.as_str());
+            // app.sort_column = app.all_info_columns[app.selected_column.unwrap()].column;
+            return;
+        }
         FloatingWidget::AddTorrentConfirm => return,
         _ => (),
     }
@@ -131,15 +131,22 @@ fn handle_right(app: &mut App) {
 }
 
 fn handle_left(app: &mut App) {
-    if let FloatingWidget::AddTorrentConfirm = app.floating_widget {
-        app.floating_widget = FloatingWidget::AddTorrent;
-    }
-
-    match app.last_route_focused_widget() {
-        Some(FocusableWidget::Tabs) => {
-            app.previous_tab();
+    match app.floating_widget {
+        FloatingWidget::AddTorrentConfirm => {
+            app.floating_widget = FloatingWidget::AddTorrent;
+            return;
+        }
+        FloatingWidget::ModifyColumns => {
+            let column = app.all_info_columns[app.selected_column.unwrap()].column;
+            app.sort_column = column;
+            app.sort_descending = false;
+            return;
         }
         _ => (),
+    }
+
+    if let Some(FocusableWidget::Tabs) = app.last_route_focused_widget() {
+        app.previous_tab();
     }
 }
 
@@ -152,18 +159,12 @@ fn handle_help(app: &mut App) {
 }
 
 async fn handle_pause(app: &mut App) {
-    match app.floating_widget {
-        FloatingWidget::AddTorrentConfirm => {
-            app.toggle_add_torrent_paused();
-            return;
-        }
-        _ => (),
+    if let FloatingWidget::AddTorrentConfirm = app.floating_widget {
+        app.toggle_add_torrent_paused();
+        return;
     }
-    match app.last_route_focused_widget() {
-        Some(FocusableWidget::TorrentList) => {
-            app.toggle_torrent_pause().await;
-        }
-        _ => (),
+    if let Some(FocusableWidget::TorrentList) = app.last_route_focused_widget() {
+        app.toggle_torrent_pause().await;
     }
 }
 
@@ -172,22 +173,16 @@ async fn handle_rename(app: &mut App) {
         return;
     }
 
-    match app.last_route_focused_widget() {
-        Some(FocusableWidget::TorrentList) => {
-            app.floating_widget = FloatingWidget::Input;
-            app.input_mode = InputMode::Editing;
-        }
-        _ => (),
+    if let Some(FocusableWidget::TorrentList) = app.last_route_focused_widget() {
+        app.floating_widget = FloatingWidget::Input;
+        app.input_mode = InputMode::Editing;
     }
 }
 
 async fn handle_add(app: &mut App) {
-    match app.last_route_focused_widget() {
-        Some(FocusableWidget::TorrentList) => {
-            app.floating_widget = FloatingWidget::AddTorrent;
-            app.get_torrent_files();
-        }
-        _ => (),
+    if let Some(FocusableWidget::TorrentList) = app.last_route_focused_widget() {
+        app.floating_widget = FloatingWidget::AddTorrent;
+        app.get_torrent_files();
     }
 }
 
@@ -196,20 +191,14 @@ async fn handle_remove(app: &mut App) {
         return;
     }
 
-    match app.last_route_focused_widget() {
-        Some(FocusableWidget::TorrentList) => {
-            app.floating_widget = FloatingWidget::RemoveTorrent;
-        }
-        _ => (),
+    if let Some(FocusableWidget::TorrentList) = app.last_route_focused_widget() {
+        app.floating_widget = FloatingWidget::RemoveTorrent;
     }
 }
 
 fn handle_toggle(app: &mut App) {
-    match app.floating_widget {
-        FloatingWidget::RemoveTorrent => {
-            app.delete_files = !app.delete_files;
-        }
-        _ => (),
+    if let FloatingWidget::RemoveTorrent = app.floating_widget {
+        app.delete_files = !app.delete_files;
     }
 }
 
@@ -219,6 +208,16 @@ fn handle_columns(app: &mut App) {
     }
 
     app.floating_widget = FloatingWidget::ModifyColumns;
+}
+
+async fn handle_verify(app: &mut App) {
+    if !matches!(app.floating_widget, FloatingWidget::None) {
+        return;
+    }
+
+    if let FocusableWidget::TorrentList = app.last_route_focused_widget().unwrap() {
+        app.verify_torrent().await;
+    }
 }
 
 async fn handle_enter(app: &mut App) {
@@ -237,15 +236,12 @@ async fn handle_enter(app: &mut App) {
         _ => (),
     }
 
-    match app.last_route_focused_widget() {
-        Some(FocusableWidget::FileList) => {
-            app.torrent_collapse_files[app.selected_torrent_file.unwrap()].collapse =
-                !app.torrent_collapse_files[app.selected_torrent_file.unwrap()].collapse;
-            for file in &app.torrent_collapse_files {
-                log::error!("{} | {}", file.collapse, file.path.path().to_str().unwrap());
-            }
+    if let Some(FocusableWidget::FileList) = app.last_route_focused_widget() {
+        app.torrent_collapse_files[app.selected_torrent_file.unwrap()].collapse =
+            !app.torrent_collapse_files[app.selected_torrent_file.unwrap()].collapse;
+        for file in &app.torrent_collapse_files {
+            log::error!("{} | {}", file.collapse, file.path.path().to_str().unwrap());
         }
-        _ => (),
     }
 }
 
